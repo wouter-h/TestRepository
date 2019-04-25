ecef3d937b151321f2adf71942594c92614f382c
wouter-h <w.hertsenberg@student.rug.nl>
2019-04-25 16:17:29 +0200

    creating testBranch 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 import java.util.*;
 
 public class HelloWorld{
+	
+	public void uselessFunction() {
+		System.out.println("I am a useless function");
+	}
+	
 	public static main(String[] args){
 		System.out.println("Hello World!");
 	}
